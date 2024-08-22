3aee12ccd8021fd9eb5c5a24fac41b3608caf149
Do Nhat <dmnhat.pt@gmail.com>
2024-08-22 14:53:18 +0700

    Extending the User entity.

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,6 +4,17 @@ public class AppUser
 {
     public int Id { get; set; }
     public required string UserName { get; set; }
-    public required byte[] PasswordHashed { get; set; }
-    public required byte[] PasswordSalt { get; set; }
+    public byte[] PasswordHashed { get; set; } = [];
+    public byte[] PasswordSalt { get; set; } = [];
+    public DateOnly DateOfBirth { get; set; }
+    public required string KnownAs { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public required string Gender { get; set; }
+    public string? Introduction { get; set; }
+    public string? Interests { get; set; }
+    public string? LookingFor { get; set; }
+    public required string City { get; set; }
+    public required string Country { get; set; }
+    public List<Photo> Photos { get; set; } = [];
 }
