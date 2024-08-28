1604ad312048a2d0587b5cab8698dc3246d383b1
Do Nhat <dmnhat.pt@gmail.com>
2024-08-28 16:05:42 +0700

    Creating the member edit component.

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { AccountService } from '../../_services/account.service';
+import { MembersService } from '../../_services/members.service';
+import { Member } from '../../_models/member';
+
+@Component({
+  selector: 'app-member-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './member-edit.component.html',
+  styleUrl: './member-edit.component.css',
+})
+export class MemberEditComponent implements OnInit {
+  private accountService = inject(AccountService);
+  private memberService = inject(MembersService);
+
+  member?: Member;
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const user = this.accountService.currentUser();
+    if (!user) return;
+    this.memberService
+      .getMember(user.username)
+      .subscribe((member) => (this.member = member));
+  }
+}
