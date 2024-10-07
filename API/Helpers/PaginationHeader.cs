c5e18dd6235d44482d6f1cefe264beb9c5fe34a8
Do Nhat <dmnhat.pt@gmail.com>
2024-10-07 15:07:00 +0700

    Adding helper classes for pagination.

---
+++ API/Helpers/PaginationHeader.cs
@@ -0,0 +1,9 @@
+namespace API.Helpers;
+
+public class PaginationHeader(int currentPage, int itemPerPage, int totalItems, int totalPages)
+{
+    public int CurrentPage { get; set; } = currentPage;
+    public int ItemPerPage { get; set; } = itemPerPage;
+    public int TotalItems { get; set; } = totalItems;
+    public int TotalPages { get; set; } = totalPages;
+}
