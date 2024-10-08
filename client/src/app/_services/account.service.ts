8410220c6951e51fd8eeca812cbfb95335f05981
Do Nhat <dmnhat.pt@gmail.com>
2024-10-08 16:13:43 +0700

    Setting up the likes functions in the Angular app.

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -3,18 +3,20 @@ import { inject, Injectable, signal } from '@angular/core';
 import { User } from '../_models/user';
 import { map } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { LikesService } from './likes.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
   private http = inject(HttpClient);
+  private likesService = inject(LikesService);
 
   baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
 
   login(model: any) {
-    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+    return this.http.post<User>(`${this.baseUrl}account/login`, model).pipe(
       map((user) => {
         if (user) this.setCurrentUser(user);
       })
@@ -22,7 +24,7 @@ export class AccountService {
   }
 
   register(model: any) {
-    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+    return this.http.post<User>(`${this.baseUrl}account/register`, model).pipe(
       map((user) => {
         if (user) this.setCurrentUser(user);
         return user;
@@ -33,6 +35,7 @@ export class AccountService {
   setCurrentUser(user: User) {
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUser.set(user);
+    this.likesService.getLikeIds();
   }
 
   logOut() {
