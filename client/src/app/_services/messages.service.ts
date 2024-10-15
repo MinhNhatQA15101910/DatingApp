72c715fa2a4f245f17a9ebf7e9c67c0f8640f0fb
Do Nhat <dmnhat.pt@gmail.com>
2024-10-15 09:53:41 +0700

    Setting up the Angular app for messaging.

---
+++ client/src/app/_services/messages.service.ts
@@ -0,0 +1,32 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '../_models/message';
+import { PaginatedResult } from '../_models/pagination';
+import { setPaginatedResponse, setPaginationHeaders } from './paginationHelper';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessagesService {
+  private http = inject(HttpClient);
+
+  baseUrl = environment.apiUrl;
+  paginatedResult = signal<PaginatedResult<Message[]> | null>(null);
+
+  getMessages(container: string, pageNumber: number, pageSize: number) {
+    let params = setPaginationHeaders(pageNumber, pageSize);
+
+    params = params.append('container', container);
+
+    return this.http
+      .get<Message[]>(`${this.baseUrl}messages`, {
+        observe: 'response',
+        params,
+      })
+      .subscribe({
+        next: (response) =>
+          setPaginatedResponse(response, this.paginatedResult),
+      });
+  }
+}
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MessagesService } from '../_services/messages.service';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
   imports: [],
   templateUrl: './messages.component.html',
-  styleUrl: './messages.component.css'
+  styleUrl: './messages.component.css',
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
+  messagesService = inject(MessagesService);
 
+  container = 'Inbox';
+  pageNumber = 1;
+  pageSize = 5;
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messagesService.getMessages(
+      this.container,
+      this.pageNumber,
+      this.pageSize
+    );
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 }
