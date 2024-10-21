b47f4cab62b0b18d0a5cb1dc49fa91e10578ffcd
Do Nhat <dmnhat.pt@gmail.com>
2024-10-21 22:10:42 +0700

    Designing the inbox.

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,10 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MessagesService } from '../_services/messages.service';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
+import { TimeagoModule } from 'ngx-timeago';
+import { Message } from '../_models/message';
+import { RouterLink } from '@angular/router';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonsModule,
+    FormsModule,
+    TimeagoModule,
+    RouterLink,
+    PaginationModule,
+  ],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css',
 })
@@ -25,6 +37,14 @@ export class MessagesComponent implements OnInit {
       this.pageNumber,
       this.pageSize
     );
+  }
+
+  getRoute(message: Message) {
+    if (this.container === 'Outbox') {
+      return `/members/${message.recipientUsername}`;
+    }
+
+    return `/members/${message.senderUsername}`;
   }
 
   pageChanged(event: any) {
