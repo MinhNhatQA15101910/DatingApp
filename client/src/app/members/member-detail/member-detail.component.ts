1e9fef105efee51c3447f7053d729b59ce79b3d5
Do Nhat <dmnhat.pt@gmail.com>
2024-10-22 10:00:23 +0700

    Using route resolvers.

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -28,14 +28,22 @@ export class MemberDetailComponent implements OnInit {
   private messagesService = inject(MessagesService);
   private route = inject(ActivatedRoute);
 
-  @ViewChild('memberTabs') memberTabs?: TabsetComponent;
-  member?: Member;
+  @ViewChild('memberTabs', { static: true }) memberTabs?: TabsetComponent;
+  member: Member = {} as Member;
   images: GalleryItem[] = [];
   activeTab?: TabDirective;
   messages: Message[] = [];
 
   ngOnInit(): void {
-    this.loadMember();
+    this.route.data.subscribe({
+      next: (data) => {
+        this.member = data['member'];
+        this.member &&
+          this.member.photos.map((p) =>
+            this.images.push(new ImageItem({ src: p.url, thumb: p.url }))
+          );
+      },
+    });
 
     this.route.queryParams.subscribe({
       next: (params) => {
@@ -66,16 +74,5 @@ export class MemberDetailComponent implements OnInit {
         },
       });
     }
-  }
-
-  loadMember() {
-    const username = this.route.snapshot.paramMap.get('username');
-    if (!username) return;
-    this.memberService.getMember(username).subscribe((member) => {
-      this.member = member;
-      member.photos.map((p) =>
-        this.images.push(new ImageItem({ src: p.url, thumb: p.url }))
-      );
-    });
   }
 }
