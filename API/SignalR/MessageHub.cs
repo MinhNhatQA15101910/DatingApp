fb80a83fd81f736773bf06230a33f45ecfcaef8c
Do Nhat <dmnhat.pt@gmail.com>
2025-01-28 15:39:26 +0700

    Creating a message hub.

---
+++ API/SignalR/MessageHub.cs
@@ -0,0 +1,37 @@
+
+
+using API.Extensions;
+using API.Interfaces;
+
+namespace API.SignalR;
+
+public class MessageHub(IMessageRepository messageRepository) : Hub
+{
+    public override async Task OnConnectedAsync()
+    {
+        var httpContext = Context.GetHttpContext();
+        var otherUser = httpContext?.Request.Query["user"];
+
+        if (Context.User == null || string.IsNullOrEmpty(otherUser))
+        {
+            throw new HubException("Cannot join group");
+        }
+
+        var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
+        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+        var messages = await messageRepository.GetMessageThreadAsync(Context.User.GetUsername(), otherUser!);
+        await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        return base.OnDisconnectedAsync(exception);
+    }
+
+    private static string GetGroupName(string caller, string? other)
+    {
+        var stringCompare = string.CompareOrdinal(caller, other) < 0;
+        return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+    }
+}
