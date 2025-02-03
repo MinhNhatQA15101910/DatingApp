b4cdc6b3ce252f4e2e6e09106f055b1c43abad8d
Do Nhat <dmnhat.pt@gmail.com>
2025-02-03 17:20:01 +0700

    Done FE for photo management.

--- client/src/app/_services/admin.service.ts
+++ client/src/app/_services/admin.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { User } from '../_models/user';
+import { Photo } from '../_models/photo';
 
 @Injectable({
   providedIn: 'root',
@@ -11,8 +12,20 @@ export class AdminService {
 
   baseUrl = environment.apiUrl;
 
+  approvePhoto(photoId: number) {
+    return this.http.post(`${this.baseUrl}admin/approve-photo/${photoId}`, {});
+  }
+
+  getPhotosForApproval() {
+    return this.http.get<Photo[]>(`${this.baseUrl}admin/photos-to-moderate`);
+  }
+
   getUsersWithRoles() {
     return this.http.get<User[]>(`${this.baseUrl}admin/users-with-roles`);
+  }
+
+  rejectPhoto(photoId: number) {
+    return this.http.post(`${this.baseUrl}admin/reject-photo/${photoId}`, {});
   }
 
   updateUserRoles(username: string, roles: string[]) {
--- client/src/app/admin/photo-management/photo-management.component.ts
+++ client/src/app/admin/photo-management/photo-management.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AdminService } from '../../_services/admin.service';
+import { Photo } from '../../_models/photo';
 
 @Component({
   selector: 'app-photo-management',
   standalone: true,
   imports: [],
   templateUrl: './photo-management.component.html',
-  styleUrl: './photo-management.component.css'
+  styleUrl: './photo-management.component.css',
 })
-export class PhotoManagementComponent {
+export class PhotoManagementComponent implements OnInit {
+  private adminService = inject(AdminService);
 
+  photos: Photo[] = [];
+
+  approvePhoto(photoId: number) {
+    this.adminService.approvePhoto(photoId).subscribe({
+      next: (_) => (this.photos = this.photos.filter((x) => x.id !== photoId)),
+    });
+  }
+
+  getPhotosForApproval() {
+    this.adminService.getPhotosForApproval().subscribe({
+      next: (photos) => (this.photos = photos),
+    });
+  }
+
+  ngOnInit(): void {
+    this.getPhotosForApproval();
+  }
+
+  rejectPhoto(photoId: number) {
+    this.adminService.rejectPhoto(photoId).subscribe({
+      next: (_) => (this.photos = this.photos.filter((x) => x.id !== photoId)),
+    });
+  }
 }
